4a71f75c9c31b600ecad7f24f04f3cf34c8fe1c6
Robin SHAMSNEJAD <rsham@free.fr>
2023-06-27 21:38:34 +0200

    Bump

--- railcraft_kiosk.lua
+++ railcraft_kiosk.lua
@@ -1,12 +1,22 @@
 destinations = {
+    "House",
     "NetherMeteor",
     "DesertJungle",
     "VolcanoVillage",
     "Creek"
 }
-destinations[0] = "House"
+--destinations[0] = "House"
 
+if true then
+    print("Available destinations:")
 
-for i = 0, #destinations do
-    print(destinations[i])
+    for i = 1, #destinations do
+        print("\t"..i..": "..destinations[i])
+    end
+
+    io.write("\nYour choice? [1-"..#destinations.."] ")
+
+    user_input = io.read("*n")
+
+    print("Choo choo too "..destinations[user_input].."!")
 end
