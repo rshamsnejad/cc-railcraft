98f8c99be64aff272875445e542a20d8170a80f8
Robin SHAMSNEJAD <robin.shamsnejad@arturia.com>
2023-06-29 16:35:55 +0200

    Add inventory management

--- rc.lua
+++ rc.lua
@@ -12,14 +12,28 @@ local destinations = {
     "VolcanoVillage",
     "Creek"
 }
+local destinations_inv = {}
+for key, value in pairs(destinations) do
+    destinations_inv[value] = key
+end
+
 local buttons = {}
 local buttonHeight = 1
 local destinationPrefix = "Current destination: "
 
 local viewport = viewportAPI.new({term = term})
+chest_id = "minecraft:chest"
+track_id = "railcraft:routing_track"
+local chest = peripheral.find(chest_id)
+local track = peripheral.find(track_id)
 
 buttonHandler = function(element, x, y)
+    -- Before we change it, statusbtn.text contains the previous destination,
+    -- which should be the one of the ticket already in place
+    track.pushItems(chest_id, 1, 1, destinations_inv[statusbtn.text])
+
     statusbtn.text = destinationPrefix..element.text
+    chest.pushItems(track_id, destinations_inv[element.text], 1)
 
     return true -- requests redraw of current viewport
 end
